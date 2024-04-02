5109f06456eb00875acddd11ef700fab23fdc0d6
Grupo CedNet <lvluansantos@gmail.com>
2024-04-02 09:35:35 -0300

    Atualizado editor

--- resources/views/pages/admin/src/pages/EditorNewPost/Main.jsx
+++ resources/views/pages/admin/src/pages/EditorNewPost/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EditorNewPost } from '.';
-// import 'react-quill/dist/quill.snow.css';
 
 if (document.getElementById('create-news-editor')) {
   const container = ReactDOM.createRoot(
