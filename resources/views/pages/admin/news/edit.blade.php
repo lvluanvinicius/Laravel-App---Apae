46873e721c1558d72a0e078f05703ae7f18b45d5
Grupo CedNet <lvluansantos@gmail.com>
2024-04-02 15:11:41 -0300

    Removido restrição unique de transparency_folders

--- resources/views/pages/admin/news/edit.blade.php
+++ resources/views/pages/admin/news/edit.blade.php
@@ -1,7 +1,5 @@
 <x-admin.app-default app_title="" page_title="{{ $title }}">
-    @section('head')
-        <link rel="stylesheet" href="https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css">
-    @endsection
+
     @section('content')
         <div id="news-edit" data-news-id="{{ request('newsId') }}"></div>
     @endsection
--- resources/views/pages/admin/src/pages/EditorNewPost/Main.jsx
+++ resources/views/pages/admin/src/pages/EditorNewPost/Main.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EditorNewPost } from '.';
 
+import '../../styles/quill.snow.css';
+
 if (document.getElementById('create-news-editor')) {
-  console.log('====================================');
-  console.log('create-news-editor');
-  console.log('====================================');
   const container = ReactDOM.createRoot(
     document.getElementById('create-news-editor')
   );
